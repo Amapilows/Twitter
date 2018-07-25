df68640962222d10a460dc73bec10893a64e0186
DESKTOP-AGVU5J4\Javi <javiera@beerly.cl>
2018-07-24 21:55:04 -0400

    tercer commit

--- Twitter-Prueba/src/js/app.js
+++ Twitter-Prueba/src/js/app.js
@@ -1,1 +1,29 @@
-alert('hola');
+$(function(){
+
+	$('.card').click('click', function(){
+		$(this).toggleClass('card--open')
+	});
+
+	$('.card__like').click('click', function(e){
+		$(this).toggleClass('card__like--red');
+		e.preventDefault();
+		e.stopPropagation();
+	});
+
+	$('.card__follow-btn').click('click', function(e){
+		$(this).toggleClass('card__follow-btn--following');
+		e.preventDefault();
+		e.stopPropagation();
+	});
+
+	$("#image").on("change", function(e){
+	$(".create__image img").attr("src", "src/img/squared/" + $(this).val());
+	});
+
+	$("#author").on("change", function(){
+	$(".create__profile img").attr("src", "src/img/profiles/" + $(this).val());
+	});
+
+	$
+	
+});
