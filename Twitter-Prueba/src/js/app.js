8c4f8419945bc64d1209075fc203d51281f41192
DESKTOP-AGVU5J4\Javi <javiera@beerly.cl>
2018-07-26 21:34:51 -0400

    cambios

--- Twitter-Prueba/src/js/app.js
+++ Twitter-Prueba/src/js/app.js
@@ -5,16 +5,18 @@ require ('bootstrap');
 $('document').ready(function(){
 	$("#push__Tweet").on('submit', function(event){
 		var text = $("#myTweet").val();
+		var img = $("#create__profile").val(); 
 		event.preventDefault();
 		$(".new__post").prepend(
 			'<div class="new__twitter container">'
-			+'<div class="new__twitter--white">'
-			+'<img>'
-			+'<i class="fas fa-trash-alt button__delete">'
-			+'<i class="fas fa-heart"></i>'
+			+ '<div class="new__twitter--white">'
+			+ '<img src="../src/img/profiles/" class="create__img">'
+			+ img
+			+ '<i class="fas fa-trash-alt button__delete">'
+			+ '<i class="fas fa-heart card__like" id="counter"></i>'
 			+ text
-			+'</div>'
-			+'</div>');
+			+ '</div>'
+			+ '</div>');
 	});
 
 	$('#new_tweet').on('click', function (){
@@ -22,7 +24,7 @@ $('document').ready(function(){
 	});
 		
 	$('.card__like').click('click', function(e){
-		$(this).toggleClass('card__like--red');
+		$(this).addClass('card__like--red');
 		e.preventDefault();
 		e.stopPropagation();
 	});
@@ -35,6 +37,13 @@ $('document').ready(function(){
 	$("#author").on("change", function(){
 	$(".create__profile img").attr("src", "../src/img/profiles/" + $(this).val());
 	});
+
+	$(".post__like").on('click', function(){
+		counter = $("#counter").text();
+		counter =parseInt(counter);
+		console.log(counter + 1);
+		$('#counter').text(counter + 1);
+	})
 
 	
 	
